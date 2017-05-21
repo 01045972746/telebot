3ff06ee9076394af47b76598dc0aa14ac1184991
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-05-21 20:47:23 +0900

    test
    test

--- src/main/java/org/telegram/telebot/controller/MyWebhook.java
+++ src/main/java/org/telegram/telebot/controller/MyWebhook.java
@@ -1,10 +1,7 @@
 package org.telegram.telebot.controller;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,18 +10,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.telegram.telebot.model.Document;
 import org.telegram.telebot.model.Update;
-import org.telegram.telebot.model.methods.SendDocument;
 import org.telegram.telebot.model.methods.SendMessage;
 import org.telegram.telebot.service.TelegramBotService;
 import org.telegram.telebot.service.exceptions.FailResponseMethodException;
+import org.telegram.telebot.service.myservices.MyTelegramService;
 
 @Controller
 public class MyWebhook {
 
 	@Autowired
 	TelegramBotService service;
+	
+	@Autowired
+	MyTelegramService myservice;
 	
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
@@ -47,15 +46,8 @@ public class MyWebhook {
 				service.sendMessage(msg);
 				break;
 			case "/getTest":
-				URL url = new URL("https://downloads.meetinggear.com/prod_data/webapps/upload/board/2017/02/27/0a42469b-373b-4636-a625-c83ebcc8152a.pdf");
-				String tDir = System.getProperty("java.io.tmpdir");
-				String path = tDir + "tmp" + ".pdf";
-				File file = new File(path);
-				file.deleteOnExit();
-				FileUtils.copyURLToFile(url, file);
-				SendDocument sd = new SendDocument();
-				sd.setFileToSend(file);
-				service.sendDocument(sd);
+				byte[] test = myservice.test();
+				System.out.println(test.toString());
 				break;
 		}
 		
--- src/main/java/org/telegram/telebot/service/myservices/MyTelegramService.java
+++ src/main/java/org/telegram/telebot/service/myservices/MyTelegramService.java
@@ -1,5 +1,12 @@
 package org.telegram.telebot.service.myservices;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.springframework.stereotype.Service;
 import org.telegram.telebot.service.TelegramBotService;
 
@@ -11,5 +18,40 @@ public class MyTelegramService extends TelegramBotService{
 		String myBotToken = "304855908:AAHkGwCOIwdEY2JPlXNpqC5KDiW6b-iCBWE";
 		return myBotToken;
 	}
+	
+	public byte[] test() throws IOException {
+		URL u = new URL("http://www.java2s.com/binary.dat");
+	    URLConnection uc = u.openConnection();
+	    String contentType = uc.getContentType();
+	    int contentLength = uc.getContentLength();
+	    if (contentType.startsWith("text/") || contentLength == -1) {
+	      throw new IOException("This is not a binary file.");
+	    }
+	    InputStream raw = uc.getInputStream();
+	    InputStream in = new BufferedInputStream(raw);
+	    byte[] data = new byte[contentLength];
+	    int bytesRead = 0;
+	    int offset = 0;
+	    while (offset < contentLength) {
+	      bytesRead = in.read(data, offset, data.length - offset);
+	      if (bytesRead == -1)
+	        break;
+	      offset += bytesRead;
+	    }
+	    in.close();
+	
+	    if (offset != contentLength) {
+	      throw new IOException("Only read " + offset + " bytes; Expected " + contentLength + " bytes");
+	    }
+	    String filename ="";
+	    filename = u.getFile().substring(filename.lastIndexOf('/') + 1);
+	    FileOutputStream out = new FileOutputStream(filename);
+	    out.write(data);
+	    out.flush();
+	    out.close();
+	    
+	    return data;
+	  }
+	
 
 }
