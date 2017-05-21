b475d67c82302e708d6ce3378a612d27444d65de
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-05-21 20:30:33 +0900

    testsendfile
    testsendfile

--- src/main/java/org/telegram/telebot/controller/MyWebhook.java
+++ src/main/java/org/telegram/telebot/controller/MyWebhook.java
@@ -1,7 +1,10 @@
 package org.telegram.telebot.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.telegram.telebot.model.Document;
 import org.telegram.telebot.model.Update;
 import org.telegram.telebot.model.methods.SendDocument;
 import org.telegram.telebot.model.methods.SendMessage;
@@ -24,7 +28,7 @@ public class MyWebhook {
 	
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void myWebhook(@RequestBody Update update) throws FailResponseMethodException {
+	public void myWebhook(@RequestBody Update update) throws FailResponseMethodException, IOException {
 		if(update != null) {
 		int chat_id = update.getMessage().getChat().getId();
 		if(chat_id < 0) {
@@ -43,7 +47,9 @@ public class MyWebhook {
 				service.sendMessage(msg);
 				break;
 			case "/getTest":
-				File file = new File("https://downloads.meetinggear.com/prod_data/webapps/upload/board/2017/02/27/0a42469b-373b-4636-a625-c83ebcc8152a.pdf");
+				URL url = new URL("https://downloads.meetinggear.com/prod_data/webapps/upload/board/2017/02/27/0a42469b-373b-4636-a625-c83ebcc8152a.pdf");
+				File file = new File("");
+				FileUtils.copyURLToFile(url, file);
 				SendDocument sd = new SendDocument();
 				sd.setFileToSend(file);
 				service.sendDocument(sd);
