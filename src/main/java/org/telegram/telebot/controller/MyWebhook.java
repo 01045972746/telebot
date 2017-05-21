61c797947bf69fffa668b4e7eff9705cd5fdd6be
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-05-21 20:12:29 +0900

    switch
    switch

--- src/main/java/org/telegram/telebot/controller/MyWebhook.java
+++ src/main/java/org/telegram/telebot/controller/MyWebhook.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.telegram.telebot.model.Document;
+import org.telegram.telebot.model.File;
 import org.telegram.telebot.model.Update;
 import org.telegram.telebot.model.methods.SendMessage;
 import org.telegram.telebot.service.TelegramBotService;
 import org.telegram.telebot.service.exceptions.FailResponseMethodException;
+import org.telegram.telebot.service.myservices.MyTelegramService;
 
 @Controller
 public class MyWebhook {
@@ -19,36 +22,33 @@ public class MyWebhook {
 	@Autowired
 	TelegramBotService service;
 	
+	private MyTelegramService myservice;
+	
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void myWebhook(@RequestBody Update update) throws FailResponseMethodException {
-		System.out.println(update.getCallback_query());
 		if(update != null) {
 		int chat_id = update.getMessage().getChat().getId();
 		if(chat_id < 0) {
 			return;
 		}
 		String chat_text = update.getMessage().getText();
-		System.out.println(update.getMessage().getChat().getId());
-		System.out.println(update.getMessage().getChat().getFirst_name());
-		System.out.println(update.getMessage().getText());
-		
-		
-		if(chat_text.equals("/welcome")) {
-			SendMessage msg = new SendMessage();
-			msg.setChat_id(chat_id);
-			msg.setText("Hi, "+update.getMessage().getChat().getFirst_name());
-			service.sendMessage(msg);
-			return;
+		SendMessage msg = new SendMessage();
+		msg.setChat_id(chat_id);
+		switch(chat_text) {
+			case "/welcome":
+				msg.setText("Hi, "+update.getMessage().getChat().getFirst_name());
+				service.sendMessage(msg);
+				break;
+			case "/getToken":
+				msg.setText(myservice.getTokenBot());
+				service.sendMessage(msg);
+				break;
+			case "/getTest":
+			
+				break;
 		}
 		
-		if(chat_text.equals("/test1")) {
-			SendMessage msg = new SendMessage();
-			msg.setChat_id(chat_id);
-			msg.setText("Hi, Mister John Doetest");
-			service.sendMessage(msg);
-			return;
-		}
 		
 		
 		}
